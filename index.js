5dc88d566a774f3e4245ed2aa2ec59498034273a
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-03-06 23:03:43 -0600

    Changed css

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let weather = {
         .then((response) => {
           if (!response.ok) {
             /*alert("Can't find the city");*/
+            //document.querySelector(".error").innerText = "ERROR: Can't find the city, (Refresh page to search)";
             document.body.innerHTML = "ERROR: Can't find the city, (Refresh page to search)";
             throw new Error("Can't find the city");
           }
@@ -51,8 +52,33 @@ let weather = {
       }
     });
   
+    // var div  = document.getElementById("location");
+    // function getLocation() {
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(showPosition, showError);
+    //   } else {
+    //     div.innerHTML = "The Browser Does not Support Geolocation";
+    //   }
+    // }
+    
+    // function showPosition(position) {
+    //   fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + 
+    //   position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=fa314a00ae1a42dde73d24c64bfd3d5a")
+    //     .then(response => response.json())
+    //     .then(data => console.log(data)),
+    //   search: function () {
+    //     this.fetchWeather(document.querySelector(".search-bar").value);
+    //   }
+      
+    // }
+    
+    // function showError(error) {
+    //   if(error.PERMISSION_DENIED){
+    //       div.innerHTML = "The User have denied the request for Geolocation.";
+    //   }
+    // }
+    // getLocation();
 
-
-  weather.fetchWeather("arlington");
+  weather.fetchWeather("new york");
 
 
